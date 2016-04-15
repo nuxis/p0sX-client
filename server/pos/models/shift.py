0400ad4d32ee754c3d9109074f2193933911ff40
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-16 00:30:02 +0200

    comments out unused model

--- server/pos/models/shift.py
+++ server/pos/models/shift.py
@@ -12,13 +12,13 @@ class Shift(models.Model):
         return 'Skift ledet av ' + str(self.leader) + ' som startet ' + self.start.strftime('%Y-%m-%d %H:%M:%S')
 
 
-class Declaration(models.Model):
-    responsible = models.ForeignKey(User)
-    checked_by = models.ForeignKey(User)
-    incomming_balance = models.IntegerField()
-    revenue = models.IntegerField()
-    outgoing_balance = models.IntegerField()
-
-    @property
-    def difference(self):
-        return self.outgoing_balance - self.incomming_balance - self.revenue
+# class Declaration(models.Model):
+#     responsible = models.ForeignKey(User)
+#     checked_by = models.ForeignKey(User)
+#     incomming_balance = models.IntegerField()
+#     revenue = models.IntegerField()
+#     outgoing_balance = models.IntegerField()
+ 
+#     @property
+#     def difference(self):
+#         return self.outgoing_balance - self.incomming_balance - self.revenue
