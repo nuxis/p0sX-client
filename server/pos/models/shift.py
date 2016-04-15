bee6be5045b9a63d69de84ec4752fc2dee99c01d
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-15 23:58:43 +0200

    adds initial declaration model

--- server/pos/models/shift.py
+++ server/pos/models/shift.py
@@ -10,3 +10,15 @@ class Shift(models.Model):
 
     def __str__(self):
         return 'Skift ledet av ' + str(self.leader) + ' som startet ' + self.start.strftime('%Y-%m-%d %H:%M:%S')
+
+
+class Declaration(models.Model):
+    responsible = models.ForeignKey(User)
+    checked_by = models.ForeignKey(User)
+    incomming_balance = models.IntegerField()
+    revenue = models.IntegerField()
+    outgoing_balance = models.IntegerField()
+
+    @property
+    def difference(self):
+        return self.outgoing_balance - self.incomming_balance - self.revenue
