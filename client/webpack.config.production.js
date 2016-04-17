572562ae1e11136b92d8eb30c035d55a473ff263
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-17 20:38:28 +0200

    restructures build and webpack to follow https://github.com/chentsulin/electron-react-boilerplate

---
+++ client/webpack.config.production.js
@@ -0,0 +1,45 @@
+import webpack from 'webpack';
+import baseConfig from './webpack.config.base';
+
+const config = {
+  ...baseConfig,
+
+  devtool: 'source-map',
+
+  entry: './app/app',
+
+  output: {
+    ...baseConfig.output,
+
+    publicPath: '../build/'
+  },
+
+  module: {
+    ...baseConfig.module,
+
+    loaders: [
+        baseConfig.module.loaders
+    ]
+  },
+
+  plugins: [
+    ...baseConfig.plugins,
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      __DEV__: false,
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+    //new webpack.optimize.UglifyJsPlugin({
+    //  compressor: {
+    //    screw_ie8: true,
+    //    warnings: false
+    //  }
+    //})
+  ],
+
+  target: 'electron-renderer'
+};
+
+export default config;
