808b75cc158800bbeda77ccfcfd2a2031a4514ac
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-18 01:36:32 +0200

    adds request wrapper with api token handling for ajax

---
+++ client/app/request-wrapper.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+import settings from './settings'
+
+const HEADERS = {
+  'Authorization': `Token ${settings.get('api_auth_token')}`
+}
+
+const BASE_URL = settings.get('server_address')
+
+let get = (endpoint) => {
+  let url = `${BASE_URL}${endpoint}`
+  let options = {
+    headers: HEADERS,
+    timeout: 2000
+  }
+  return axios.get(url, options)
+}
+
+let post = (endpoint, data) => {
+  let url = `${BASE_URL}${endpoint}`
+  let options = {
+    headers: HEADERS,
+    timeout: 2000,
+    data: data
+  }
+  return axios.post(url, options)
+}
+
+module.exports = {
+  get,
+  post
+}
--- client/app/settings.js
+++ client/app/settings.js
@@ -2,9 +2,6 @@
 import ElectronSettings from 'electron-settings'
 import { remote } from 'electron'
 
-console.log(`${__dirname}/settings.json`)
-console.log(remote.app.getAppPath())
-
 module.exports = new ElectronSettings({
     configDirPath: remote.app.getAppPath(),
     configFileName: 'settings'
