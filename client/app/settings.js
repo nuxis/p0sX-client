09f93b4d19205bafcc6f49ae3b8f013f46d2afe2
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-17 21:54:19 +0200

    adds settings file example

--- client/app/settings.js
+++ client/app/settings.js
@@ -1,7 +1,11 @@
 'use strict'
 import ElectronSettings from 'electron-settings'
-import remote from 'remote'
+import { remote } from 'electron'
 
-const app = remote.app
+console.log(`${__dirname}/settings.json`)
+console.log(remote.app.getAppPath())
 
-module.exports = new ElectronSettings()
+module.exports = new ElectronSettings({
+    configDirPath: remote.app.getAppPath(),
+    configFileName: 'settings'
+})
