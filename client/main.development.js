b18271cab8b259b3c5c5470fc8704aa664df6f4f
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-17 21:44:17 +0200

    removes settings from mainjs

--- client/main.development.js
+++ client/main.development.js
@@ -1,9 +1,6 @@
 import { app, BrowserWindow } from 'electron'
-import ElectronSettings from 'electron-settings';
 
 let mainWindow = null
-
-let settings = null
 
 app.on('window-all-closed', () => {
     if (process.platform != 'darwin') {
@@ -14,9 +11,6 @@ app.on('window-all-closed', () => {
 app.on('ready', () => {
     // Initialize the window to our specified dimensions
     mainWindow = new BrowserWindow({ width: 1200, height: 900, icon: __dirname + '/static/pi_logo.png', frame: true});
-
-    settings = new ElectronSettings()
-    settings.set('server_address', 'http://127.0.0.1:8000')
 
     // Tell Electron where to load the entry point from
     mainWindow.loadURL('file://' + __dirname + '/app/app.html');
