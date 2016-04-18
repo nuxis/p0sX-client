56d48b671a3400d3497d621e019fd3ead73e6148
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-18 02:26:44 +0200

    makes card id/nfc primary_key of user

--- server/pos/models/user.py
+++ server/pos/models/user.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class User(models.Model):
+    card = models.CharField(max_length=255, unique=True, primary_key=True)
     name = models.CharField(max_length=255)
     max_credit = models.IntegerField()
-    card = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
