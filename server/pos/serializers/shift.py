b3377b2fc9cfda65da5bbc9554361dfa854255e9
Håvard Lian <haavarhl@stud.ntnu.no>
2016-04-19 18:25:16 +0200

    Removed server for client repo

--- server/pos/serializers/shift.py
+++
@@ -1,9 +0,0 @@
-from pos.models.shift import Shift
-
-from rest_framework import serializers
-
-
-class ShiftSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Shift
-        fields = ('id', 'start', 'end', 'leader')
--- server/pos/serializers/user.py
+++
@@ -1,9 +0,0 @@
-from pos.models.user import User
-
-from rest_framework import serializers
-
-
-class CustomerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('name', 'max_credit', 'card')
