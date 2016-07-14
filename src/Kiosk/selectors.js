7496f3b495cba9b6eeebb283a2e017c5f2b6e85d
Kristoffer Dalby <kradalby@kradalby.no>
2016-07-14 18:28:28 +0200

    adds selectors for total price and filter items based on selected category

--- src/Kiosk/selectors.js
+++ src/Kiosk/selectors.js
@@ -8,6 +8,30 @@ export const getCurrentItem = (state) => state.currentItem
 
 export const getCart = (state) => state.cart
 
+export const getTotalPriceOfCart = createSelector(
+    [getCart],
+    (cart) => {
+        return cart.reduce((accumulator, entry) => {
+            accumulator += entry.item.get('price')
+            accumulator += entry.ingredients.reduce((accumulator, ingredient) => {
+                return accumulator + parseInt(ingredient.get('price'))
+            }, 0)
+            return accumulator
+        }, 0)
+    }
+)
+
 export const getIngredients = (state) => state.ingredients
 
 export const getItems = (state) => state.items
+
+export const getItemsByCategory = createSelector(
+    [getItems, getSelectedCategory],
+    (items, categoryId) => {
+        if (categoryId === 0) {
+            return items
+        } else {
+            return items.filter((item) => item.get('category') === categoryId)
+        }
+    }
+)
