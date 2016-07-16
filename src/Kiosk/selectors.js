47bc400b5ec47c70ad7945c49d1421f8b22330d2
Kristoffer Dalby <kradalby@kradalby.no>
2016-07-16 10:37:07 +0200

    adds selector for rendering cartentry prices correctly

--- src/Kiosk/selectors.js
+++ src/Kiosk/selectors.js
@@ -8,12 +8,28 @@ export const getCurrentItem = (state) => state.currentItem
 
 export const getCart = (state) => state.cart
 
+// This method should possibly be renamed and used when
+// when retrieveing the cart for checkout?
+export const getRenderedCart = createSelector(
+    [getCart],
+    (cart) => {
+        return cart.map((entry) => {
+            const item = entry.get('item')
+            const priceWithIngredients = entry.get('ingredients').reduce((total, ingredient) => {
+                return total + parseInt(ingredient.get('price'))
+            }, item.get('price'))
+            return entry.set('item', item.set('price', priceWithIngredients))
+        })
+    }
+)
+
 export const getTotalPriceOfCart = createSelector(
     [getCart],
     (cart) => {
         return cart.reduce((accumulator, entry) => {
-            accumulator += entry.item.get('price')
-            accumulator += entry.ingredients.reduce((accumulator, ingredient) => {
+            console.log('getpriceselector: ', entry)
+            accumulator += entry.get('item').get('price')
+            accumulator += entry.get('ingredients').reduce((accumulator, ingredient) => {
                 return accumulator + parseInt(ingredient.get('price'))
             }, 0)
             return accumulator
@@ -28,6 +44,7 @@ export const getItems = (state) => state.items
 export const getItemsByCategory = createSelector(
     [getItems, getSelectedCategory],
     (items, categoryId) => {
+        console.log('getItemsByCategory: ', items, categoryId)
         if (categoryId === 0) {
             return items
         } else {
