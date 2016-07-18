4836fdd35446f15cc94068aade6ff59b060b14cc
Kristoffer Dalby <kradalby@kradalby.no>
2016-07-18 07:43:19 +0200

    adds kitchen sagas

--- src/sagas.js
+++ src/sagas.js
@@ -1,11 +1,15 @@
 import { fork } from 'redux-saga/effects'
-import { watchKioskData, watchItems, watchCategories, watchPostPurchase } from './Kiosk/sagas'
+import { watchKioskData, watchItems, watchIngredients, watchCategories, watchPostPurchase } from './Kiosk/sagas'
+import { loopGetOrders } from './Kitchen/sagas'
 
 export default function * root () {
     yield [
         fork(watchKioskData),
         fork(watchItems),
         fork(watchCategories),
-        watchPostPurchase()
+        watchPostPurchase(),
+        fork(watchIngredients),
+        fork(watchCategories),
+        fork(loopGetOrders)
     ]
 }
