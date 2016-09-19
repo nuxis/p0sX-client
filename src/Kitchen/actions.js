6013b35a0a1d7a54e3952188096d60fb26557042
Kristoffer Dalby <kradalby@kradalby.no>
2016-09-19 17:43:34 +0200

    adds actions for setting orders

--- src/Kitchen/actions.js
+++ src/Kitchen/actions.js
@@ -3,7 +3,11 @@ function action (type, payload = {}) {
 }
 
 // Action definitions
-export const SET_ORDERS = 'SET_ORDERS'
+export const SET_OPEN_ORDERS = 'SET_OPEN_ORDERS'
+export const SET_DONE_ORDERS = 'SET_DONE_ORDERS'
+export const SET_ARCHIVED_ORDERS = 'SET_ARCHIVED_ORDERS'
 
 // Action creators
-export const setOrders = (orders) => action(SET_ORDERS, { orders })
+export const setOpenOrders = (orders) => action(SET_OPEN_ORDERS, { orders })
+export const setDoneOrders = (orders) => action(SET_DONE_ORDERS, { orders })
+export const setArchivedOrders = (orders) => action(SET_ARCHIVED_ORDERS, { orders })
