57120aab10c0563ef44ad589d110731f5b28824b
Kristoffer Dalby <kradalby@kradalby.no>
2016-09-19 17:44:32 +0200

    adds sagas for fetching orders

--- src/Kitchen/sagas.js
+++ src/Kitchen/sagas.js
@@ -4,20 +4,39 @@ import * as api from '../common/api'
 import * as actions from './actions'
 import settings from '../common/settings'
 
-const DELAY = parseInt(settings.get('customer_order_delay')) || 5
+const DELAY = parseInt(settings.get('customer_order_delay')) || 10
 
 export function * loopGetOrders () {
     while (true) {
         console.log('DELAY :', DELAY)
-        yield delay(DELAY * 5000)
-        yield call(getOrders)
+        yield delay(DELAY * 1000)
+        yield call(getOpenOrders)
+        yield call(getDoneOrders)
     }
 }
 
-function * getOrders () {
+function * getOpenOrders () {
     try {
-        const orders = yield call(api.getOrders)
-        yield put(actions.setOrders(orders))
+        const orders = yield call(api.getOrders, api.ORDER_STATE.OPEN)
+        yield put(actions.setOpenOrders(orders))
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function * getDoneOrders () {
+    try {
+        const orders = yield call(api.getOrders, api.ORDER_STATE.DONE)
+        yield put(actions.setDoneOrders(orders))
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function * getArchivedOrders () {
+    try {
+        const orders = yield call(api.getOrders, api.ORDER_STATE.ARCHIVED)
+        yield put(actions.setArchivedOrders(orders))
     } catch (error) {
         console.error(error)
     }
