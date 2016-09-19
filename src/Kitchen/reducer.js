0abe9b13e8469305d4dea7900c244dbd27b99053
Kristoffer Dalby <kradalby@kradalby.no>
2016-09-19 17:44:46 +0200

    adds orders to reducer

--- src/Kitchen/reducer.js
+++ src/Kitchen/reducer.js
@@ -1,13 +1,21 @@
-import { List } from 'immutable'
+import { Map, List } from 'immutable'
 import * as actions from './actions'
 
-const orderInit = List()
+const orderInit = Map({
+    open: List(),
+    done: List(),
+    archived: List()
+})
 
 export function orders (state = orderInit, action) {
     console.log('order state: ', state, action)
     switch (action.type) {
-    case actions.SET_ORDERS:
-        return action.orders
+    case actions.SET_OPEN_ORDERS:
+        return state.set('open', action.orders)
+    case actions.SET_DONE_ORDERS:
+        return state.set('done', action.orders)
+    case actions.SET_ARCHIVED_ORDERS:
+        return state.set('archived', action.orders)
     default:
         return state
     }
