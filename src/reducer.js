3d6a279dcbdbc8a8711dcb15e8b5c032e301eb87
Håvard Lian <haavarhl@stud.ntnu.no>
2016-09-20 17:06:11 +0200

    Linting

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
-import { categories, selectedCategory, currentItem, cart, ingredients, items, search, payment } from './Kiosk/reducer'
+import { categories, selectedCategory, currentItem, cart, items, search, payment } from './Kiosk/reducer'
 
 export default combineReducers({
     items,
