267358778c17b9f999747329cd083f04d4034d95
Kristoffer Dalby <kradalby@kradalby.no>
2016-09-28 16:09:06 +0200

    adds missing import

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
-import { categories, selectedCategory, currentItem, cart, items, search, payment, cashier, discounts, creditCheck } from './Kiosk/reducer'
+import { categories, selectedCategory, currentItem, cart, items, search, payment, cashier, discounts, creditCheck, lastOrder } from './Kiosk/reducer'
 
 export default combineReducers({
     items,
